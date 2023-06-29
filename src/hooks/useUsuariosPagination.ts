06bdbf02093062e03d346c46ba4b57f6178a27dd
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-29 10:22:02 -0500

    cambiando la paginacion

--- src/hooks/useUsuariosPagination.ts
+++ src/hooks/useUsuariosPagination.ts
@@ -1,17 +1,18 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Usuarios } from '@/interfaces/fetchUsuariosResponse';
 import { PageType } from '@/types';
 
 // Puedes usar un tipo más específico aquí si necesitas validaciones adicionales.
 
-export const useUsuariosPagination = (page: PageType) => {
+export const useUsuariosPagination = (page: PageType = 1) => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [usuarios, setUsuarios] = useState<Usuarios>();
 	const [count, setCount] = useState(0);
 
-	useMemo(() => {
+	useEffect(() => {
 		setIsLoading(true); // Configura isLoading a true cada vez que page cambia.
+
 		axios.get(`/api/clientes/${page}`).then(({ data }) => {
 			setUsuarios(data.results);
 			setCount(Math.ceil(data.count / 9));
