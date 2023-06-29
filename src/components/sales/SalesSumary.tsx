273bbb5d7406813b66ec379cd3e4bbd6a4646b91
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-29 08:17:06 -0500

    verificando deploy

--- src/components/sales/SalesSumary.tsx
+++ src/components/sales/SalesSumary.tsx
@@ -6,7 +6,7 @@ import Money from '../svgs/Money';
 import Truck from '../svgs/Truck';
 import CityScape from '../svgs/CityScape';
 import SalesItems from './SalesItemsProps';
-import BarsPrueva from '../Charts/BarChart';
+import BarsPrueva from '../charts/BarChart';
 
 interface SalesSumaryProps {
 	resumen?: Resumen;
