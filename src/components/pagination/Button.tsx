159800c09cdc9821c7a24b50fe12e54870418a3d
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-29 10:44:55 -0500

    paginacion clientes terminada

--- src/components/pagination/Button.tsx
+++ src/components/pagination/Button.tsx
@@ -6,12 +6,12 @@ function Button({ content, onClick, active, disabled }: ButtonProps) {
 			className={`flex flex-col cursor-pointer items-center justify-center w-9 h-9 shadow-[0_4px_10px_rgba(0,0,0,0.03)] text-sm font-normal transition-colors rounded-lg
       ${
 				active
-					? 'bg-primary text-white dark:bg-primaryDark'
+					? 'bg-pink-400  dark:bg-primaryDark text-white'
 					: 'text-primary dark:text-primaryDark'
 			}
       ${
 				!disabled
-					? 'bg-white hover:bg-primary hover:text-white dark:hover:bg-primaryDark'
+					? 'bg-white hover:bg-pink-400 hover:text-white dark:hover:bg-primaryDark'
 					: 'text-zinc-400 dark:text-zinc-400 bg-white cursor-not-allowed '
 			}
       `}
