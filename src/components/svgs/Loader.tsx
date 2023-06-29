c911139ede01826a69c943a2813bf07dce84d41d
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-29 13:36:03 -0500

    verificando build

---
+++ src/components/svgs/Loader.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+interface L4SVGProps {
+	className?: string;
+}
+
+const L4: React.FC<L4SVGProps> = ({ className }) => {
+	return (
+		<svg
+			version='1.0'
+			xmlns='http://www.w3.org/2000/svg'
+			width='100px'
+			height='100px'
+			viewBox='0 0 100 100'
+			preserveAspectRatio='xMidYMid meet'
+			className={`t ${className}`}
+		>
+			<circle
+				fill='#fff'
+				stroke='none'
+				cx='6'
+				cy='50'
+				r='6'
+			>
+				<animate
+					attributeName='opacity'
+					dur='1s'
+					values='0;1;0'
+					repeatCount='indefinite'
+					begin='0.1'
+				/>
+			</circle>
+			<circle
+				fill='#fff'
+				stroke='none'
+				cx='26'
+				cy='50'
+				r='6'
+			>
+				<animate
+					attributeName='opacity'
+					dur='1s'
+					values='0;1;0'
+					repeatCount='indefinite'
+					begin='0.2'
+				/>
+			</circle>
+			<circle
+				fill='#fff'
+				stroke='none'
+				cx='46'
+				cy='50'
+				r='6'
+			>
+				<animate
+					attributeName='opacity'
+					dur='1s'
+					values='0;1;0'
+					repeatCount='indefinite'
+					begin='0.3'
+				/>
+			</circle>
+		</svg>
+	);
+};
+
+export default L4;
