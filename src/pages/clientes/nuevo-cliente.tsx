2f64401448b092a7f7aa150c2ab1bcabd0c246ce
John Santamaria <36826659+JohnRSantamaria@users.noreply.github.com>
2023-06-29 23:04:39 -0500

    arreglando el boton

--- src/pages/clientes/nuevo-cliente.tsx
+++ src/pages/clientes/nuevo-cliente.tsx
@@ -96,7 +96,7 @@ const Newclient: React.FC = () => {
 			/>
 
 			<main className='flex flex-col justify-start items-center pt-8 w-full min-h-[calc(100vh-100px)]'>
-				<section className='flex flex-col gap-16  w-3/5  '>
+				<section className='flex flex-col gap-16  w-3/4  p-8 shadow-lg'>
 					<form
 						onSubmit={handleSubmit}
 						className='flex flex-col justify-center items-center gap-6 mt-6  '
@@ -131,7 +131,7 @@ const Newclient: React.FC = () => {
 							options={cuidades}
 						/>
 
-						<button className='w-[260px] ml-auto bg-zinc-100 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-xl shadow active:relative active:top-[1px] sm:mr-auto mb-4'>
+						<button className='w-[220px] ml-auto bg-zinc-100 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-xl shadow active:relative active:top-[1px] sm:mr-auto mb-4'>
 							Crear
 						</button>
 					</form>
